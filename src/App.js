f26c43b79c77f057b711d3cd7e67a237d833462a
Alfred <kalumunabisimwa5@gmail.com>
2022-05-26 16:44:27 +0200

    correct all requiements get by the reviewer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import HomePage from './components/HomePage';
-import CovidData from './components/CovidData';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Layout from './Layout';
+import Home from './components/Home';
+import Details from './components/Details';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/data/:name" element={<CovidData />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/:Country" index element={<Details />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
